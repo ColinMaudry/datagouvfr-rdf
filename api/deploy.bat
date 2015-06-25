5254dfeea46f088d441fea099f1199354572f4bb
Colin Maudry <colin@maudry.com>
2015-06-25 19:45:08 +0200

    When deploying API config, also copy web.xml

--- api/deploy.bat
+++ api/deploy.bat
@@ -2,6 +2,7 @@
 set tomcat=C:\Users\colin\programs\apache-tomcat-7.0.57\
 set tomcatapp=%tomcat%webapps\ROOT\
 cd C:\Users\colin\CloudStation\work\linkeddata\datagouvfr-rdf\api
+copy WEB-INF\web.xml %tomcatapp%WEB-INF
 copy specs\*.ttl %tomcatapp%specs
 copy xslt\*.xsl %tomcatapp%lda-assets\xslt
 copy css\* %tomcatapp%lda-assets\css
